b19d33818549224ce697eef623f9743fec09f0d1
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-09 16:55:02 +0700

    Make Forecast activity

--- app/src/main/java/com/hskris/weathermvp/ui/forecast/ForecastActivity.kt
+++ app/src/main/java/com/hskris/weathermvp/ui/forecast/ForecastActivity.kt
@@ -1,12 +1,13 @@
-package com.hskris.weathermvp
+package com.hskris.weathermvp.ui.forecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hskris.weathermvp.R
 
-class MainActivity : AppCompatActivity() {
+class ForecastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_forecast)
     }
 }
