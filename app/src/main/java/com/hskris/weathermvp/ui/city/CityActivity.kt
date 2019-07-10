1e6f206e0075152af4bed6e121fb62c91b1e2fd3
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-10 15:10:09 +0700

    Create city activity

---
+++ app/src/main/java/com/hskris/weathermvp/ui/city/CityActivity.kt
@@ -0,0 +1,13 @@
+package com.hskris.weathermvp.ui.city
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hskris.weathermvp.R
+
+class CityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city)
+    }
+}
