33d55ba2f9caa49bc758cbcf1507eab4681e1d1e
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-15 14:05:37 +0700

    Fix koin module bug

--- app/src/main/java/com/hskris/weathermvp/di/app_modules.kt
+++ app/src/main/java/com/hskris/weathermvp/di/app_modules.kt
@@ -1,12 +1,14 @@
 package com.hskris.weathermvp.di
 
+import com.hskris.weathermvp.data.models.CityForecast
+import com.hskris.weathermvp.ui.UseCase
 import com.hskris.weathermvp.ui.forecast.ForecastContract
 import com.hskris.weathermvp.ui.forecast.ForecastPresenter
 import com.hskris.weathermvp.ui.forecast.domain.usecase.GetForecast
 import org.koin.dsl.module
 
 val weatherAppModule = module {
-    single { GetForecast() }
-    factory { (view: ForecastContract.View) -> ForecastPresenter(get(), view) as ForecastContract.Presenter}
+    single<UseCase<Int, CityForecast>> { GetForecast() }
+    factory<ForecastContract.Presenter> { (view: ForecastContract.View) -> ForecastPresenter(get(), view)}
 
 }
