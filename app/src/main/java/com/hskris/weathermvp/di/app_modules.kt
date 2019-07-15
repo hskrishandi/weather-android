afae0f94d6eaa9717299e02fab2aadae248f5577
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-15 14:08:19 +0700

    Dependency injection for use case get forecast

--- app/src/main/java/com/hskris/weathermvp/di/app_modules.kt
+++ app/src/main/java/com/hskris/weathermvp/di/app_modules.kt
@@ -1,6 +1,9 @@
 package com.hskris.weathermvp.di
 
 import com.hskris.weathermvp.data.models.CityForecast
+import com.hskris.weathermvp.data.repository.CityForecastRepository
+import com.hskris.weathermvp.data.repository.remote.CityForecastRemoteRepository
+import com.hskris.weathermvp.data.repository.remote.api.Api
 import com.hskris.weathermvp.ui.UseCase
 import com.hskris.weathermvp.ui.forecast.ForecastContract
 import com.hskris.weathermvp.ui.forecast.ForecastPresenter
@@ -8,7 +11,8 @@ import com.hskris.weathermvp.ui.forecast.domain.usecase.GetForecast
 import org.koin.dsl.module
 
 val weatherAppModule = module {
-    single<UseCase<Int, CityForecast>> { GetForecast() }
+    single<CityForecastRepository> { CityForecastRemoteRepository(Api.getInstance())}
+    single<UseCase<Int, CityForecast>> { GetForecast(get()) }
     factory<ForecastContract.Presenter> { (view: ForecastContract.View) -> ForecastPresenter(get(), view)}
 
 }
--- app/src/main/java/com/hskris/weathermvp/ui/forecast/domain/usecase/GetForecast.kt
+++ app/src/main/java/com/hskris/weathermvp/ui/forecast/domain/usecase/GetForecast.kt
@@ -2,13 +2,9 @@ package com.hskris.weathermvp.ui.forecast.domain.usecase
 
 import com.hskris.weathermvp.data.models.CityForecast
 import com.hskris.weathermvp.data.repository.CityForecastRepository
-import com.hskris.weathermvp.data.repository.remote.CityForecastRemoteRepository
-import com.hskris.weathermvp.data.repository.remote.api.Api
 import com.hskris.weathermvp.ui.UseCase
 
 class GetForecast(private val repository: CityForecastRepository) : UseCase<Int, CityForecast> {
-
-    constructor(): this(CityForecastRemoteRepository(Api.getInstance()))
 
     override fun execute(requestValues: Int, callback: UseCase.UseCaseCallback<CityForecast>) {
 
