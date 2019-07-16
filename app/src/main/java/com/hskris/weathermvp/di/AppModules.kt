6ac60570fb0cbab7e2b86a4697293014746ae27f
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-16 16:47:50 +0700

    Remove unused imports

--- app/src/main/java/com/hskris/weathermvp/di/AppModules.kt
+++ app/src/main/java/com/hskris/weathermvp/di/AppModules.kt
@@ -1,11 +1,8 @@
 package com.hskris.weathermvp.di
 
-import com.hskris.weathermvp.data.models.City
-import com.hskris.weathermvp.data.models.CityForecast
 import com.hskris.weathermvp.data.repository.CityForecastRepository
 import com.hskris.weathermvp.data.repository.remote.CityForecastRemoteRepository
 import com.hskris.weathermvp.data.repository.remote.api.Api
-import com.hskris.weathermvp.ui.UseCase
 import com.hskris.weathermvp.ui.city.CityContract
 import com.hskris.weathermvp.ui.city.CityPresenter
 import com.hskris.weathermvp.ui.city.domain.usecase.GetCity
