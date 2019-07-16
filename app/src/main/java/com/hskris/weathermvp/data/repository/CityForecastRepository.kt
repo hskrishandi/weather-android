68a89f331fce1e2ddb88292bec15b4aa67542649
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-16 16:49:40 +0700

    Remove parsing logic from repository

--- app/src/main/java/com/hskris/weathermvp/data/repository/CityForecastRepository.kt
+++ app/src/main/java/com/hskris/weathermvp/data/repository/CityForecastRepository.kt
@@ -1,10 +1,10 @@
 package com.hskris.weathermvp.data.repository
 
-import com.hskris.weathermvp.data.models.CityForecast
+import com.hskris.weathermvp.data.repository.remote.models.CityForecastResponse
 
 interface CityForecastRepository {
     interface ResponseListener {
-        fun onResponse(cityForecast: CityForecast)
+        fun onResponse(response: CityForecastResponse)
     }
 
     fun fetchForecastByCityId(id: Int, listener: ResponseListener)
--- app/src/main/java/com/hskris/weathermvp/data/repository/remote/CityForecastRemoteRepository.kt
+++ app/src/main/java/com/hskris/weathermvp/data/repository/remote/CityForecastRemoteRepository.kt
@@ -1,16 +1,12 @@
 package com.hskris.weathermvp.data.repository.remote
 
 import android.util.Log
-import com.hskris.weathermvp.data.models.City
-import com.hskris.weathermvp.data.models.CityForecast
-import com.hskris.weathermvp.data.models.Forecast
 import com.hskris.weathermvp.data.repository.CityForecastRepository
 import com.hskris.weathermvp.data.repository.remote.api.ApiService
 import com.hskris.weathermvp.data.repository.remote.models.CityForecastResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.util.Date
 
 class CityForecastRemoteRepository(private val api : ApiService) : CityForecastRepository {
 
@@ -24,38 +20,11 @@ class CityForecastRemoteRepository(private val api : ApiService) : CityForecastR
 
             override fun onResponse(call: Call<CityForecastResponse>, response: Response<CityForecastResponse>) {
                 response.body()?.let {
-                    val parsed = parseResponseToModel(it)
-                    listener.onResponse(parsed)
+                    listener.onResponse(it)
                 }
             }
 
         })
 
-    }
-
-    private fun parseResponseToModel(response: CityForecastResponse): CityForecast {
-
-        val cityResponse = response.city
-        val city = City (
-            cityResponse.id,
-            cityResponse.name,
-            cityResponse.country,
-            cityResponse.timezone
-        )
-
-        val cityForecast = CityForecast(city)
-
-        val forecastList = response.list
-        for(forecast in forecastList){
-            val date = forecast.dt
-            val temp = forecast.main.temp
-            val humidity = forecast.main.humidity
-            val weather = forecast.weather[0].main
-            val desc = forecast.weather[0].description
-
-            cityForecast.addForecasts(Forecast(Date(date * 1000), temp, humidity, weather, desc))
-        }
-
-        return cityForecast
     }
 }
